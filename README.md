1e4dc62726eac4f09f075f112a99b005f8eb546e
Paul Kernfeld <paulkernfeld@gmail.com>
2018-08-09 23:26:13 +0100

    Add a doctest example

--- README.md
+++ README.md
@@ -4,4 +4,21 @@ This package contains an implementation of
 [BFGS](https://en.wikipedia.org/w/index.php?title=BFGS_method), an algorithm for minimizing
 convex twice-differentiable functions.
 
+In this example, we minimize a 2d function:
+
+```rust
+extern crate bfgs;
+extern crate ndarray;
+
+use ndarray::prelude::*;
+
+fn main() {
+    let x0 = Array::from_vec(vec![8.888, 1.234]);  // Chosen arbitrarily
+    let f = |x: &Array1<f64>| x.dot(x);
+    let g = |x: &Array1<f64>| 2.0 * x;
+    let x_min = bfgs::bfgs(x0, f, g);
+    assert_eq!(x_min, Ok(Array::from_vec(vec![0.0, 0.0])));
+}
+```
+
 License: MIT/Apache-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,23 @@
 //! This package contains an implementation of
 //! [BFGS](https://en.wikipedia.org/w/index.php?title=BFGS_method), an algorithm for minimizing
 //! convex twice-differentiable functions.
+//!
+//! In this example, we minimize a 2d function:
+//!
+//! ```rust
+//! extern crate bfgs;
+//! extern crate ndarray;
+//!
+//! use ndarray::prelude::*;
+//!
+//! fn main() {
+//!     let x0 = Array::from_vec(vec![8.888, 1.234]);  // Chosen arbitrarily
+//!     let f = |x: &Array1<f64>| x.dot(x);
+//!     let g = |x: &Array1<f64>| 2.0 * x;
+//!     let x_min = bfgs::bfgs(x0, f, g);
+//!     assert_eq!(x_min, Ok(Array::from_vec(vec![0.0, 0.0])));
+//! }
+//! ```
 #[cfg(not(test))]
 extern crate ndarray;
 #[cfg(test)]
@@ -145,15 +162,6 @@ mod tests {
         let g = |x: &Array1<f64>| -2.0 * x;
         let x_min = bfgs(x0, f, g);
         assert_eq!(x_min, Err(()));
-    }
-
-    #[test]
-    fn test_x_squared_2d() {
-        let x0 = array![2.0, 2.0];
-        let f = |x: &Array1<f64>| x.iter().map(|xx| xx * xx).sum();
-        let g = |x: &Array1<f64>| 2.0 * x;
-        let x_min = bfgs(x0, f, g);
-        assert_eq!(x_min, Ok(array![0.0, 0.0]));
     }
 
     #[test]
